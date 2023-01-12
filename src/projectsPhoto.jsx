aa7eae6fc106e3325f8c9fdae532aff2267ca295
stevenrhett <johnsonsteven18@gmail.com>
2023-01-12 17:39:37 -0500

    Created project photo page, services page, and team page.Next step is to import the react-router-dom and start to code the routes of the page

---
+++ src/projectsPhoto.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default function ProjectsPhoto(){
+    return (
+        <div>
+            <div className="w-80 carousel rounded-box">
+                <div className="carousel-item w-full">
+                    <img src="https://placeimg.com/256/400/arch" className="w-full" alt="Tailwind CSS Carousel component" />
+                </div>
+                <div className="carousel-item w-full">
+                    <img src="https://placeimg.com/256/400/arch" className="w-full" alt="Tailwind CSS Carousel component" />
+                </div>
+                <div className="carousel-item w-full">
+                    <img src="https://placeimg.com/256/400/arch" className="w-full" alt="Tailwind CSS Carousel component" />
+                </div>
+                <div className="carousel-item w-full">
+                    <img src="https://placeimg.com/256/400/arch" className="w-full" alt="Tailwind CSS Carousel component" />
+                </div>
+                <div className="carousel-item w-full">
+                    <img src="https://placeimg.com/256/400/arch" className="w-full" alt="Tailwind CSS Carousel component" />
+                </div>
+                <div className="carousel-item w-full">
+                    <img src="https://placeimg.com/256/400/arch" className="w-full" alt="Tailwind CSS Carousel component" />
+                </div>
+                <div className="carousel-item w-full">
+                    <img src="https://placeimg.com/256/400/arch" className="w-full" alt="Tailwind CSS Carousel component" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
