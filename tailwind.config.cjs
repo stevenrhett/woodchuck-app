ce1e1d169450840e48b7cbedf225d40d772284f2
stevenrhett <johnsonsteven18@gmail.com>
2023-02-16 12:08:16 -0500

    finished working on the main page and the footer. Next I will work on the stumps page which is a photo gallary page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,8 @@
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",], theme: {
         screens: {
-            'sm': {'min': '370px', 'max': '767px'},
-            'md': {'min': '768px', 'max': '1023px'},
-            'lg': {'min': '1024px', 'max': '1280px'},
-            'xl': {'min': '1281px', 'max': '1600px'},
+            'sm': {'min': '370px', 'max': '745px'},
+            'lg': {'min': '746px', 'max': '2400px'},
         },
     }, plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms'), require("daisyui")],
 };
