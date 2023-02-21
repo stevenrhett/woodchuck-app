f89f8ee1f1d733e7421c0b747f9912ca7a509f9e
stevenrhett <johnsonsteven18@gmail.com>
2023-02-21 08:47:04 -0500

    the opening page is done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",], theme: {
         screens: {
-            'sm': {'min': '576px', 'max': '768px'}, 'lg': {'min': '769px', 'max': '2400px'},
+            'sm': {'min': '375px', 'max': '768px'}, 'lg': {'min': '769px', 'max': '2400px'},
         },
     }, plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/forms'), require("daisyui")],
 };
